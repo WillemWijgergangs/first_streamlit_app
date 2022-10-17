37198dea6c1e65b7b02067c5f8003090ed0cad67
Willem Wijgergangs <34240324+WillemWijgergangs@users.noreply.github.com>
2022-10-17 13:25:02 +0200

    Update streamlit_app.py
    Fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 # display fruityvice api response
-streamlit.header('Fruityvie Fruit Advice!')
+streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
